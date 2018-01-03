6532f0a78085be6a639b761113cecfe00eb5ea8c
Dylan Stein <djstein@ncsu.edu>
2018-01-03 14:08:12 -0500

    add email verification api endpoint

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,14 +1,14 @@
-from allauth.account.views import confirm_email
 from django.urls import path, include
 from rest_framework import routers
-from project.users.views import UserViewSet, django_rest_auth_null
+from project.users.views import UserViewSet, django_rest_auth_null, VerifyEmailView
 
 router = routers.SimpleRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
+    path('registration/verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
     path('registration/', include('rest_auth.registration.urls')),
     path('rest-auth/registration/account-email-verification-sent/', django_rest_auth_null, name='account_email_verification_sent'),
-    path('registration/account-confirm-email/<str:key>/', confirm_email, name='account_confirm_email')
+    path('password-reset/confirm/<str:uidb64>)/<str:token>/', django_rest_auth_null, name='password_reset_confirm')
 ]
