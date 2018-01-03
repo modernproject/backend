4753a20357de75a7cc8100c622f30a8d4f3ecb3b
Dylan Stein <djstein@ncsu.edu>
2018-01-03 01:19:43 -0500

    posts updated with category

--- project/posts/models.py
+++ project/posts/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from django.utils.text import slugify
 
 class Post(models.Model):
+    BACKEND = 'BACKEND'
+    FRONTEND = 'FRONTEND'
+    DEPLOYMENT = 'DEPLOYMENT'
+
+    POST_CATEGORY = (
+        (BACKEND, 'Backend'),
+        (FRONTEND, 'Frontend'),
+        (DEPLOYMENT, 'Deployment'),
+    )
+
     title = models.CharField(max_length=200, unique=True, blank=False, null=False)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, default='default')
     content = models.TextField(blank=False)
+    category = models.CharField(max_length=50, choices=POST_CATEGORY, default=BACKEND)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
