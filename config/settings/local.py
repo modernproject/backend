1bd9165984371c210a8a7ca715189a797400e033
Dylan Stein <djstein@ncsu.edu>
2018-01-20 16:18:36 -0500

    update allowed hosts and cors for new nginx setup

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,7 +7,7 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8080',
+    'localhost',
 )
 
 DATABASES = {
